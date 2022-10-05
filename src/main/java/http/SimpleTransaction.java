e399bce58ab7fbe5c85e6acfbb12b72c23b2daf5
Superman <Superman@DESKTOP-H1JB47U.home>
2022-10-05 12:14:47 -0400

    rename Json to SimpleTrans

--- src/main/java/http/SimpleTransaction.java
+++ src/main/java/http/SimpleTransaction.java
@@ -5,12 +5,14 @@ import static reflection.Main.require;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Reader;
+import java.net.BindException;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -20,21 +22,57 @@ import reflection.RefCode;
 import reflection.RefException;
 import tw.util.S;
 
-class Transaction {
-	static void listen(HttpHandler handler, int port) throws Exception {
-
-		// create HTTP server w/ single thread executor
-		HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-		server.createContext("/", handler); 
-		server.setExecutor( Executors.newFixedThreadPool(5) );  // five threads but we are synchronized for single execution
-		server.start();
+class SimpleTransaction {
+	interface MyHttpHandler {
+		void handle( SimpleTransaction trans);
+	}
+	
+	private HttpExchange m_exchange;
+	
+	public SimpleTransaction( HttpExchange exchange) {
+		m_exchange = exchange;
+		;
+		int a = 3;
 	}
 
-	public static ParamMap getJson(HttpExchange exch) {
-		// TODO Auto-generated method stub
-		return null;
+	public static void listen(String host, int port, MyHttpHandler handler) {
+		try {
+			HttpServer server = HttpServer.create(new InetSocketAddress(host, port), 0);
+			server.createContext("/favicon", exch -> {} ); // ignore these requests
+			server.createContext("/", exch -> handler.handle( new SimpleTransaction( exch) ) ); 
+			server.setExecutor( Executors.newFixedThreadPool(5) );
+			server.start();
+		}
+		catch( BindException e) {
+			S.out( "The application is already running");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
+	// now write a getJsonRequest
+	String getRequest() throws Exception {
+		String uri = m_exchange.getRequestURI().toString().toLowerCase();
+		require( uri.length() < 4000, RefCode.UNKNOWN, "URI is too long");
+
+		if ("GET".equals(m_exchange.getRequestMethod() ) ) {
+			S.out( "Received GET request %s %s", uri, m_exchange.getHttpContext().getPath() ); 
+			String[] parts = uri.split("\\?");
+			return parts.length > 1 ? parts[1] : "";
+		}
+		
+		// POST request		
+		Headers headers = m_exchange.getRequestHeaders();
+		String len = headers.getFirst("content-length");
+		if (len != null) {
+			byte[] bytes = new byte[Integer.valueOf( len) ];
+			m_exchange.getRequestBody().read( bytes);
+            return new String( bytes);
+		}
+		return "";
+	}
+	
 	static ParamMap getMap(HttpExchange exchange) throws Exception {
 		String uri = exchange.getRequestURI().toString().toLowerCase();
 		require( uri.length() < 4000, RefCode.UNKNOWN, "URI is too long");
@@ -83,15 +121,15 @@ class Transaction {
 		}
 		
 		return map;
-	}
+	}	
 
 	/** Only respond once for each request
 	 *  @return true if we responded just now. */
-	static void respond( HttpExchange exchange, String response) {
+	void respond( String response) {
 		try {
-			OutputStream outputStream = exchange.getResponseBody();
-			exchange.getResponseHeaders().add( "Content-Type", "application/json");
-			exchange.sendResponseHeaders(200, response.length());
+			OutputStream outputStream = m_exchange.getResponseBody();
+			m_exchange.getResponseHeaders().add( "Content-Type", "application/json");
+			m_exchange.sendResponseHeaders(200, response.length());
 			outputStream.write(response.getBytes());
 			outputStream.close();
 		}
