0a19bf68782ccab06cc14300f7c5c54212f6dbb9
Peter at work <Superman@DESKTOP-H1JB47U.home>
2022-11-07 16:39:48 -0500

    show headers

--- src/main/java/http/EchoServer.java
+++ src/main/java/http/EchoServer.java
@@ -1,8 +1,13 @@
 package http;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import tw.util.S;
 
 public class EchoServer {
+	private static SimpleDateFormat timeFmt = new SimpleDateFormat( "HH:mm:ss.SSS");
+	
 	public static void main(String[] args) {
 		String host = args[0];
 		int port = Integer.valueOf( args[1]);
@@ -10,8 +15,19 @@ public class EchoServer {
 		
 		SimpleTransaction.listen( host, port, transaction -> {
 			try {
-				S.out( transaction.getRequest() );
-				transaction.respond( "ok");
+				String body = transaction.getRequest();  // we have to call getRequest() for all requests
+
+				if (transaction.getHeaders().size() > 0) {
+					S.out( "Headers:");
+					S.out( transaction.getHeaders() );
+				}
+				
+				if (transaction.isPost() ) {
+					S.out( "Body:");
+					S.out( body);
+				}
+				
+				transaction.respond( timeFmt.format( new Date() ) + " OK");
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
