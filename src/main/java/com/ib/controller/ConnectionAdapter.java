b1bc7e5e43c98f26e49d72b5728cf1d5ef47958d
Peter Spiro <peteraspiro@gmail.com>
2022-11-21 21:48:20 -0500

    add to repo

---
+++ src/main/java/com/ib/controller/ConnectionAdapter.java
@@ -0,0 +1,51 @@
+package com.ib.controller;
+
+import java.util.List;
+
+import com.ib.controller.ApiController.IConnectionHandler;
+
+public class ConnectionAdapter implements IConnectionHandler  {
+
+	@Override
+	public void onRecNextValidId(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onConnected() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void accountList(List<String> list) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void error(Exception e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void message(int id, int errorCode, String errorMsg, String advancedOrderRejectJson) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void show(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
