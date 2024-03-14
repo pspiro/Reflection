d7e9bcee71d631b24fa8452504f80dda1fd30fbc
Peter Spiro <peteraspiro@gmail.com>
2024-03-14 17:04:00 -0400

    add long to upperfield

--- src/main/java/tw/util/UpperField.java
+++ src/main/java/tw/util/UpperField.java
@@ -9,9 +9,6 @@ import javax.swing.text.BadLocationException;
 import javax.swing.text.PlainDocument;
 
 public class UpperField extends JTextField {
-	int m_ival;
-	double m_dval;
-	
 	public UpperField() {
 		this( null);
 	}
@@ -36,26 +33,6 @@ public class UpperField extends JTextField {
 		setText( s);
 	}
 	
-	public void setText( double v) {
-		if (v == Double.MAX_VALUE || v == 0) {
-			m_dval = v;
-			setText( null);
-		}
-		else {
-			super.setText( "" + v);
-		}
-	}
-
-	public void setText( int v) {
-		if (v == Integer.MAX_VALUE || v == 0) {
-			m_ival = v;
-			setText( null);
-		}
-		else {
-			super.setText( "" + v);
-		}
-	}
-
 	@Override public void setText(String t) {
 		super.setText(t);
 	}
@@ -64,7 +41,7 @@ public class UpperField extends JTextField {
 		try {
 			String str = super.getText();
 			return str == null || str.length() == 0
-				 ? m_dval : Double.parseDouble( super.getText().trim() );
+				 ? 0 : Double.parseDouble( super.getText().trim() );
 		}
 		catch( Exception e) {
 			return 0;
@@ -72,13 +49,18 @@ public class UpperField extends JTextField {
 	}
 
 	public int getInt() {
+		return (int)getLong();
+	}
+	
+	public long getLong() {
 		try {
 			String str = super.getText();
 			return str == null || str.length() == 0
-				 ? m_ival : Integer.parseInt( super.getText().trim() );
+				 ? 0 : Long.parseLong( super.getText().trim() );
 		}
 		catch( Exception e) {
 			return 0;
 		}
 	}
+
 }
