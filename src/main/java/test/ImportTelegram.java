802f78b7574362bc43e084c1084deb53420293a3
Peter Spiro <peteraspiro@gmail.com>
2024-03-24 21:02:14 -0400

    ImportTelegram fix

--- src/main/java/test/ImportTelegram.java
+++ src/main/java/test/ImportTelegram.java
@@ -12,13 +12,15 @@ import tw.util.NewLookAndFeel;
 import tw.util.S;
 
 public class ImportTelegram {
-	static JsonModel m_model = new JsonModel("from,text");
+	static JsonModel m_model = new JsonModel("date,id,from,from_id,reply_to_message_id,text");
 	
 	public static void main(String[] args) throws Exception {
 		NewLookAndFeel.register();
-		String folder = "C:\\Users\\RJJP\\Downloads\\Telegram Desktop\\ChatExport_2024-03-09\\";
+		String folder = "C:/temp/";
 		String filename = folder + "result.json";
 		JsonArray ar = JsonObject.readFromFile( filename).getArray("messages");
+		ar.forEach( obj -> obj.update( "text", textObj -> extract(textObj) ) ); 
+		ar.forEach( obj -> obj.update( "date", text -> text.toString().replace( 'T', ' ') ) ); 
 		
 		JsonArray ar2 = new JsonArray();
 
@@ -40,4 +42,11 @@ public class ImportTelegram {
 		ar2.writeToCsv( "c:\\temp\\ama.csv", ',', "from,text");
 		S.out( "done");
 	}
+	
+	static Object extract( Object textObj) {
+		return textObj instanceof JsonArray && ((JsonArray)textObj).size() > 0
+				? ((JsonArray)textObj).get(0)
+				: textObj;
+	}
+
 }
