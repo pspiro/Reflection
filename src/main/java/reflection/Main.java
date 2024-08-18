2122e018bad34c9a76ac4792a7c6eaeee1ece1ac
Peter Spiro <peteraspiro@gmail.com>
2024-08-17 23:58:42 -0400

    no change

--- src/main/java/reflection/Main.java
+++ src/main/java/reflection/Main.java
@@ -158,7 +158,7 @@ public class Main implements ITradeReportHandler {
 			server.createContext("/api/crypto-transactions", exch -> new BackendTransaction(this, exch, false).handleReqCryptoTransactions(exch) ); // obsolete, have frontend remove this
 			server.createContext("/api/transactions", exch -> new BackendTransaction(this, exch, false).handleReqCryptoTransactions(exch) );
 			server.createContext("/api/mywallet", exch -> new BackendTransaction(this, exch, false).handleMyWallet() );
-			server.createContext("/api/positions", exch -> new BackendTransaction(this, exch, false).handleReqPositions() ); // for My Reflection panel
+			server.createContext("/api/positions", exch -> new BackendTransaction(this, exch, false).handleReqPositions() ); // obsolete, remove
 			server.createContext("/api/positions-new", exch -> new BackendTransaction(this, exch, false).handleReqPositionsNew() ); // for My Reflection panel
 			server.createContext("/api/redemptions/redeem", exch -> new RedeemTransaction(this, exch).handleRedeem() );
 
