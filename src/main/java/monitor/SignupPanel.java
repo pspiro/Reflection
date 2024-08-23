ddf662a0cc6bd84695b343f8603b4d21eb4a1530
Peter Spiro <peteraspiro@gmail.com>
2024-08-23 17:26:07 -0400

    tooltip

--- src/main/java/monitor/SignupPanel.java
+++ src/main/java/monitor/SignupPanel.java
@@ -5,6 +5,8 @@ import java.awt.BorderLayout;
 import javax.swing.JProgressBar;
 import javax.swing.SwingUtilities;
 
+import org.json.simple.JsonObject;
+
 import common.SignupReport;
 import common.Util;
 import tw.util.S;
@@ -13,12 +15,19 @@ class SignupPanel extends JsonPanel {
 	JProgressBar bar = new JProgressBar();
 	
 	SignupPanel() {
-		super( new BorderLayout(), "created_at,email,first,last,country,referer,ip,utm_source,jotform,connected,transactions,rusd");
+		super( new BorderLayout(), "created_at,email,first,last,country,referer,ip,utm_source,user_agent,jotform,connected,transactions,rusd");
 		//add( "Signups", BorderLayout.NORTH);
 		add( bar, BorderLayout.NORTH);
 		add( m_model.createTable() );
 	}
 	
+	@Override protected String getTooltip(JsonObject row, String tag) {
+		if (tag.equals("user_agent") ) {
+			return row.getString(tag);
+		}
+		return null;
+	}
+
 	@Override protected void refresh() throws Exception {
 		bar.setValue( 0);
 		bar.setMaximum(130);  // estimate
