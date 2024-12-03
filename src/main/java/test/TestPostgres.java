80633b01e11a45fe0b87f4b28fd500f83399fdf3
Peter Spiro <peteraspiro@gmail.com>
2024-12-03 13:06:25 -0500

    test submit order

--- src/main/java/test/TestPostgres.java
+++ src/main/java/test/TestPostgres.java
@@ -2,15 +2,19 @@ package test;
 
 import reflection.Config.MultiChainConfig;
 import tw.util.S;
+import web3.NodeInstance;
 
 /** Just test that you can connect to the database. */
 public class TestPostgres {
 	public static void main(String[] args) throws Exception {
 		MultiChainConfig c1 = new MultiChainConfig();
-		c1.readFromSpreadsheet("Prod-config");
-		var node = c1.chains().polygon().node();
-		S.out( node.getBlockDateTime( node.getBlockNumber() ) );
-
+		c1.readFromSpreadsheet("prod-config");
+		
+		c1.chains().polygon().rusd().buyStockWithRusd(
+				NodeInstance.prod, 
+				1, 
+				c1.chains().polygon().getAnyStockToken(), 
+				1).waitForReceipt();
 		
 	}
 }
